8204d661d1f7c9d16868a6e8ca3999cb426bf467
elverjbeltran <elverbeltran04@gmail.com>
2025-04-04 22:43:22 -0500

    conexion base de datos

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,9 +1,12 @@
-const getCategory = (req,res)=>{
-    res.json({"Categoria":"Electrodomesticos"})
+import getConnection from "../db/db.js";
+const getCategory = async (req, res) => {
+  try {
+    const con = await getConnection();
+    const result = await con.query("select * from categorias");
+    res.json(result);
+  } catch (error) {"time lout resursrin"}
+};
 
-}
-
-export const methodHTTP ={
-    getCategory
-
-}
+export const methodHTTP = {
+  getCategory,
+};
